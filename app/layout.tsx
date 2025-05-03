edd4145d12624ee00989bad35353a9036f18d0ab
Ashish Kumar <ashishkumar@Mac.lan>
2025-05-03 23:02:34 +0530

    chore: added preview image and fixed lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
-import { Suspense } from "react"
-import GoogleAnalytics from "@/components/google-analytics"
+import { Inter } from "next/font/google";
+import type React from "react";
+import { Suspense } from "react";
 
-const inter = Inter({ subsets: ["latin"] })
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import GoogleAnalytics from "@/components/google-analytics";
+
+import "@/app/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "GitHub Profile README Builder",
@@ -33,26 +35,37 @@ export const metadata = {
     card: "summary_large_image",
     title: "GitHub Profile README Builder",
     description: "Create beautiful GitHub profile READMEs with ease",
-    images: ["https://github-profile-builder-dev-ashishk.vercel.app/og-image.png"],
+    images: [
+      "https://github-profile-builder-dev-ashishk.vercel.app/og-image.png",
+    ],
   },
-    generator: 'v0.dev'
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Suspense>{children}</Suspense>
           <Toaster />
-          {/* Add Google Analytics */}
-          <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ""} />
+          <Suspense>
+            <GoogleAnalytics
+              GA_MEASUREMENT_ID={
+                process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ""
+              }
+            />
+          </Suspense>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
