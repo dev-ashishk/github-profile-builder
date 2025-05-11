806c134b15ab2f29aad8c429050093582a68f642
Ashish Kumar <ashishkumar@Mac.lan>
2025-05-11 13:50:33 +0530

    chore: updated routes for robots and sitemap

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 export function middleware(request: NextRequest) {
   // Handle /robots.txt requests
-  if (request.nextUrl.pathname === basePath + "/robots.txt") {
+  if (request.nextUrl.pathname === "/robots.txt") {
     return NextResponse.rewrite(new URL("/api/robots.txt", request.url));
   }
 
   // Handle /sitemap.xml requests
-  if (request.nextUrl.pathname === basePath + "/sitemap.xml") {
+  if (request.nextUrl.pathname === "/sitemap.xml") {
     return NextResponse.rewrite(new URL("/api/sitemap.xml", request.url));
   }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -15,23 +14,23 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: basePath + "/sitemap.xml",
-  //       destination: "/api/sitemap.xml",
-  //     },
-  //     {
-  //       source: basePath + "/robots.txt",
-  //       destination: "/api/robots.txt",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap.xml",
+      },
+      {
+        source: "/robots.txt",
+        destination: "/api/robots.txt",
+      },
+    ];
+  },
   reactStrictMode: true,
   devIndicators: false,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  // output: "export",
 };
 
 export default nextConfig;
